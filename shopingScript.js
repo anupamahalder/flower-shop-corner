56910093f33dde018c5e1770a9c129c4f86f9374
Anupama_Halder <anupamahalder2022@gmail.com>
2023-09-01 21:29:52 +0530

    Added interactive price display when add to cart flowers

--- shopingScript.js
+++ shopingScript.js
@@ -1,3 +1,6 @@
+//Declare global variable
+let total = 0;
+
 function goToshop(){
     window.location.href = "flowershop.html";
 }
@@ -8,5 +11,22 @@ function goToHome(){
 
 //Handle click button 
 function handleClickBtn(){
-    console.log("Button clicked");
+    confole.log('hello');
+}
+function handleClickBtn(target){
+    //to get the title of flower card
+    const flowerName = target.parentNode.childNodes[3].innerText;
+    // create element 
+    const li = document.createElement('li');
+    li.innerText = flowerName;
+    const priceDiv = document.getElementById('flowers-name-div');
+    priceDiv.appendChild(li);
+
+    //get the price of flower
+    const flowerPriceString = target.parentNode.childNodes[7].innerText.split(" ")[1];
+    const flowerPrice = parseFloat(flowerPriceString);
+    total += flowerPrice;
+
+    //create element to show the price there
+    document.getElementById('totalPrice').innerText = total;
 }
